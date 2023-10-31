da755041b8d269e3c2c82426d8b2d2edf9df7a3e
modus.operandi <n.plaksin@technologysv.ru>
2023-10-31 12:51:37 +0300

    do not use LongValue when getting window_id

--- packages/desktop_multi_window/windows/window_channel.cc
+++ packages/desktop_multi_window/windows/window_channel.cc
@@ -27,7 +27,7 @@ WindowChannel::WindowChannel(
       return;
     }
     auto *args = std::get_if<flutter::EncodableMap>(call.arguments());
-    auto target_window_id = args->at(flutter::EncodableValue("targetWindowId")).LongValue();
+    auto target_window_id = std::get<int>(args->at(flutter::EncodableValue("targetWindowId")));
     auto arguments = args->at(flutter::EncodableValue("arguments"));
     handler_(window_id_, target_window_id, call.method_name(), &arguments, std::move(result));
   });
