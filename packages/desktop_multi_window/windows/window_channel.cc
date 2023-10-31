29a0da13a7fb7099a44e117384f4de64704015db
modus.operandi <n.plaksin@technologysv.ru>
2023-10-31 13:14:12 +0300

    back to longvalue....

--- packages/desktop_multi_window/windows/window_channel.cc
+++ packages/desktop_multi_window/windows/window_channel.cc
@@ -8,9 +8,10 @@
 #include <variant>
 
 std::unique_ptr<WindowChannel>
-WindowChannel::RegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar, int window_id) {
+WindowChannel::RegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar, int window_id)
+{
   auto window_registrar = flutter::PluginRegistrarManager::GetInstance()
-      ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar);
+                              ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar);
   auto channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
       window_registrar->messenger(), "mixin.one/flutter_multi_window_channel",
       &flutter::StandardMethodCodec::GetInstance());
@@ -19,34 +20,33 @@ WindowChannel::RegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar,
 
 WindowChannel::WindowChannel(
     int window_id,
-    std::unique_ptr<flutter::MethodChannel<Argument>> channel
-) : window_id_(window_id), channel_(std::move(channel)) {
-  channel_->SetMethodCallHandler([this](const flutter::MethodCall<Argument> &call, auto result) {
+    std::unique_ptr<flutter::MethodChannel<Argument>> channel) : window_id_(window_id), channel_(std::move(channel))
+{
+  channel_->SetMethodCallHandler([this](const flutter::MethodCall<Argument> &call, auto result)
+                                 {
     if (!handler_) {
       std::cout << "WindowChannel::SetMethodCallHandler: handler_ is null" << std::endl;
       return;
     }
     auto *args = std::get_if<flutter::EncodableMap>(call.arguments());
-    auto target_window_id = std::get<int32_t>(args->at(flutter::EncodableValue("targetWindowId")));
+    auto target_window_id = args->at(flutter::EncodableValue("targetWindowId")).LongValue();
     auto arguments = args->at(flutter::EncodableValue("arguments"));
-    handler_(window_id_, target_window_id, call.method_name(), &arguments, std::move(result));
-  });
+    handler_(window_id_, target_window_id, call.method_name(), &arguments, std::move(result)); });
 }
 
-WindowChannel::~WindowChannel() {
+WindowChannel::~WindowChannel()
+{
   channel_->SetMethodCallHandler(nullptr);
 }
 
 void WindowChannel::InvokeMethod(
     int from_window_id, const std::string &method,
     WindowChannel::Argument *arguments,
-    std::unique_ptr<flutter::MethodResult<Argument>> result
-) {
-  channel_->InvokeMethod(method, std::make_unique<flutter::EncodableValue>(
-      flutter::EncodableMap{
-          {flutter::EncodableValue("fromWindowId"), flutter::EncodableValue(from_window_id)},
-          {flutter::EncodableValue("arguments"), *arguments},
-      }
-  ), std::move(result));
+    std::unique_ptr<flutter::MethodResult<Argument>> result)
+{
+  channel_->InvokeMethod(method, std::make_unique<flutter::EncodableValue>(flutter::EncodableMap{
+                                     {flutter::EncodableValue("fromWindowId"), flutter::EncodableValue(from_window_id)},
+                                     {flutter::EncodableValue("arguments"), *arguments},
+                                 }),
+                         std::move(result));
 }
-
